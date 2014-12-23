37aa8af5499682164951c57deef5dd1268f3cc85
AleZMariani <zetadev@gmail.com>
2014-12-22 18:21:56 -0600

    Test github for mac

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var vertx = require('vertx');
 
 vertx.createHttpServer().requestHandler(function(req) {
-  req.response.end("Hello World!");
+  //req.response.end("Hello World!");
+  req.response.end("First program with vert.x")
 }).listen(8080, 'localhost');
