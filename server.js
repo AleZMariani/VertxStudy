bf5d92dc3c9442598815516655c08d8c2bddfa8b
AleZMariani <zetadev@gmail.com>
2015-01-29 13:03:53 -0600

    Sublime addons

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ var vertx = require('vertx');
 
 vertx.createHttpServer().requestHandler(function(req) {
   //req.response.end("Hello World!");
-  req.response.end("First program with vert.x ... First Branch inside sublime")
+  //Tring sublime package addins
+  req.response.end("First program with vert.x ... First Branch inside sublime");
 }).listen(8080, 'localhost');
